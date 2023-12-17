03e9d0cfc0379aab667d38e8b13d8cde01f00d02
berayco <55498165+berayco@users.noreply.github.com>
2023-12-18 00:19:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,67 @@
 import streamlit as st
+def sbox(input_byte):
+    SBOX = [0xC, 0x5, 0x6, 0xB, 0x9, 0x0, 0xA, 0xD, 0x3, 0xE, 0xF, 0x8, 0x4, 0x7, 0x1, 0x2]
+    return SBOX[input_byte]
 
-# S-Box ve P-Box tanımlamaları
-SBOX = [0xC, 0x5, 0x6, 0xB, 0x9, 0x0, 0xA, 0xD, 0x3, 0xE, 0xF, 0x8, 0x4, 0x7, 0x1, 0x2]
-PBOX = [0, 16, 32, 48, 1, 17, 33, 49, 2, 18, 34, 50, 3, 19, 35, 51, 4, 20, 36, 52, 5, 21, 37, 53, 6, 22, 38, 54, 7, 23, 39, 55, 8, 24, 40, 56, 9, 25, 41, 57, 10, 26, 42, 58, 11, 27, 43, 59, 12, 28, 44, 60, 13, 29, 45, 61, 14, 30, 46, 62, 15, 31, 47, 63]
+def inverse_sbox(input_byte):
+    INV_SBOX = [0x5, 0xe, 0xf, 0x8, 0xC, 0x1, 0x2, 0xD, 0xB, 0x4, 0x6, 0x3, 0x0, 0x7, 0x9, 0xA]
+    return INV_SBOX[input_byte]
 
-def sbox_layer(state, sbox):
-    new_state = 0
-    for i in range(16):
-        nibble = (state >> (i * 4)) & 0xF
-        new_state |= sbox[nibble] << (i * 4)
-    return new_state
-
-def pbox_layer(state, pbox):
-    new_state = 0
+def permute(state):
+    PBOX = [0, 16, 32, 48, 1, 17, 33, 49, 2, 18, 34, 50, 3, 19, 35, 51, 4, 20, 36, 52, 5, 21, 37, 53, 6, 22, 38, 54, 7, 23, 39, 55, 8, 24, 40, 56, 9, 25, 41, 57, 10, 26, 42, 58, 11, 27, 43, 59, 12, 28, 44, 60, 13, 29, 45, 61, 14, 30, 46, 62, 15, 31, 47, 63]
+    permuted = 0
     for i in range(64):
-        bit = (state >> i) & 0x1
-        new_state |= bit << pbox[i]
-    return new_state
+        bit = (state >> i) & 1
+        permuted |= bit << PBOX[i]
+    return permuted
 
-def key_schedule(master_key):
-    subkeys = [master_key >> 16]
-    for i in range(1, 32):
-        master_key = ((master_key & 0xFFFF) << 60) | (master_key >> 20)
-        master_key = (sbox_layer((master_key >> 76) & 0xF, SBOX) << 76) | (master_key & ((1 << 76) - 1))
-        master_key ^= i << 15
-        subkeys.append(master_key >> 16)
+def inverse_permute(state):
+    PBOX = [0, 16, 32, 48, 1, 17, 33, 49, 2, 18, 34, 50, 3, 19, 35, 51, 4, 20, 36, 52, 5, 21, 37, 53, 6, 22, 38, 54, 7, 23, 39, 55, 8, 24, 40, 56, 9, 25, 41, 57, 10, 26, 42, 58, 11, 27, 43, 59, 12, 28, 44, 60, 13, 29, 45, 61, 14, 30, 46, 62, 15, 31, 47, 63]
+    INV_PBOX = [PBOX.index(i) for i in range(64)]
+    permuted = 0
+    for i in range(64):
+        bit = (state >> i) & 1
+        permuted |= bit << INV_PBOX[i]
+    return permuted
+
+def key_schedule(key):
+    subkeys = []
+    for round in range(32):
+        subkeys.append(key >> 16)
+        key = ((key & 0xFFFF) << 60) | (key >> 20)
+        key = (sbox(key >> 76) << 76) | (key & 0x0FFFFFFFFFFFFFFF)
+        key ^= round << 15
     return subkeys
 
-def present_encrypt(plaintext, master_key):
-    keys = key_schedule(master_key)
+def present_encrypt(plaintext, key):
+    subkeys = key_schedule(key)
     state = plaintext
     for i in range(31):
-        state = sbox_layer(state ^ keys[i], SBOX)
-        state = pbox_layer(state, PBOX)
-    return state ^ keys[31]
+        state ^= subkeys[i]
+        state = sum([sbox((state >> (4 * i)) & 0xF) << (4 * i) for i in range(16)])
+        state = permute(state)
+    state ^= subkeys[31]
+    return state
 
-def present_decrypt(ciphertext, master_key):
-    keys = key_schedule(master_key)
-    inverse_pbox = [PBOX.index(x) for x in range(64)]
-    inverse_sbox = [SBOX.index(x) for x in range(16)]
+def present_decrypt(ciphertext, key):
+    subkeys = key_schedule(key)
     state = ciphertext
-    for i in range(30, -1, -1):
-        state = pbox_layer(state ^ keys[i], inverse_pbox)
-        state = sbox_layer(state, inverse_sbox)
-    return state ^ keys[0]
+    for i in range(31, 0, -1):
+        state ^= subkeys[i]
+        state = inverse_permute(state)
+        state = sum([inverse_sbox((state >> (4 * i)) & 0xF) << (4 * i) for i in range(16)])
+    state ^= subkeys[0]
+    return state
 
-# Streamlit Arayüzü
+# Test
+key = 0x00000000000000000001
+plaintext = 0x1234567890abcdef
+ciphertext = present_encrypt(plaintext, key)
+decrypted = present_decrypt(ciphertext, key)
+
+print(f"Plaintext:  {plaintext:016x}")
+print(f"Ciphertext: {ciphertext:016x}")
+print(f"Decrypted:  {decrypted:016x}")
 st.title("PRESENT Şifreleme Algoritması")
 
 operation = st.sidebar.selectbox("Mod Seçiniz", ["Şifreleme", "Şifre Çözme"])
